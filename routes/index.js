4bdfe3653d97f53ee28c83d18f4cfaacdeb39076
Brett J <jam23001@byui.edu>
2024-04-27 22:02:26 -0600

    W01 Individual Activity

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
+const getProfessionalDataController = require('../controllers/professionalData');
 
 routes.get('/', lesson1Controller.helloWorldRoute);
+routes.get('/professional', getProfessionalDataController.getData);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./controllers/db');
+
+app.use(cors());
  
-const port = 3000;
+const port = 8080;
 
 app.use('/', require('./routes'));
 
 app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});
+
+const dbPort = 3000;
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(dbPort);
+    console.log(`Connected to DB and listening on ${dbPort}`);
+  }
 });
